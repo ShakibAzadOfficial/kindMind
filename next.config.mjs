b91108264942437c16b2ba3f380a9e231e5b8147
Shakib Azad <shakibazadofficial@gmail.com>
2026-05-08 14:54:56 -0400

    Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production';
-const repoBasePath = isProd ? '/kindMind' : '';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -8,8 +5,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: repoBasePath,
-  assetPrefix: repoBasePath,
   reactStrictMode: true,
 };
 
